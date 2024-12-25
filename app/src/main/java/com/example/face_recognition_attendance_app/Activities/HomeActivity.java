540c36997081e92613bdda0e6fa4fe0b23b4bbca
zeeshan <zeeshantaj.zt@gmail.com>
2024-12-25 20:20:06 +0500

    initial commit

---
+++ app/src/main/java/com/example/face_recognition_attendance_app/Activities/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.face_recognition_attendance_app.Activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.face_recognition_attendance_app.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+    }
+}
---
+++ app/src/main/java/com/example/face_recognition_attendance_app/Activities/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.face_recognition_attendance_app.Activities;
+
+public class LoginActivity {
+}
