d1207e3dd21787e71cfb08be3e2b088e9fefd86b
zeeshan <zeeshantaj.zt@gmail.com>
2025-01-05 00:03:52 +0500

    attendance ui

--- app/src/main/java/com/example/face_recognition_attendance_app/Activities/Fragment/AttendanceHistoryFragment.java
+++ app/src/main/java/com/example/face_recognition_attendance_app/Activities/Fragment/AttendanceHistoryFragment.java
@@ -1,7 +1,25 @@
 package com.example.face_recognition_attendance_app.Activities.Fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.face_recognition_attendance_app.databinding.AttendanceHistoryFragmentBinding;
+import com.example.face_recognition_attendance_app.databinding.FragmentSignUpBinding;
 
 public class AttendanceHistoryFragment extends Fragment {
 
+    private AttendanceHistoryFragmentBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = AttendanceHistoryFragmentBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
 }
--- app/src/main/java/com/example/face_recognition_attendance_app/Activities/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/face_recognition_attendance_app/Activities/Fragment/ProfileFragment.java
@@ -1,6 +1,24 @@
 package com.example.face_recognition_attendance_app.Activities.Fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.face_recognition_attendance_app.databinding.AttendanceHistoryFragmentBinding;
+import com.example.face_recognition_attendance_app.databinding.ProfileFragmentBinding;
+
 public class ProfileFragment extends Fragment {
+    private ProfileFragmentBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = ProfileFragmentBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
 }
