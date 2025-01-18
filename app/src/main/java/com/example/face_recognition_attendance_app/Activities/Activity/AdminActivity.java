c337517529f77b27c7879a4a7e4ed0cdae1eb77d
zeeshan <zeeshantaj.zt@gmail.com>
2025-01-18 23:32:29 +0500

    admin work under process

--- app/src/main/java/com/example/face_recognition_attendance_app/Activities/Activity/AdminActivity.java
+++ app/src/main/java/com/example/face_recognition_attendance_app/Activities/Activity/AdminActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,10 +13,19 @@ import com.example.face_recognition_attendance_app.R;
 
 public class AdminActivity extends AppCompatActivity {
 
+    CardView allUserCard,downloadAttCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        allUserCard = findViewById(R.id.allUserCard);
+        downloadAttCard = findViewById(R.id.downloadAllAttendanceCard);
 
+        allUserCard.setOnClickListener(view -> {
+
+        });
+        downloadAttCard.setOnClickListener(view -> {
+
+        });
     }
 }
